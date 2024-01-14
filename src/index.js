afd9e86adbcc3477c7152e10913700aeaa54b95c
cpavansingh <cpavansingh97@gmail.com>
2024-01-14 14:29:33 +0100

    Add express server and connect to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import DBConnect from "./db/DBconnect.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./env"
 })
 
-DBConnect();
+DBConnect()
+.then(
+    app.on("Error", (err)=> {console.log("Express connection to DB failed", err);throw err}),
+    app.listen(process.env.PORT || 9000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    }))
+.catch(
+    (err)=>{console.log(`MongoDB connection error: ${err}`)}
+    )
